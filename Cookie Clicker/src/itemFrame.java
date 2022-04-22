d8b3c322607d7cd093e14f93c6202cf7a0394e8e
1903994 <1903994@01-19766.pusd.dom>
2022-04-22 12:42:16 -0700

    Fixed background spagetti code

--- Cookie Clicker/src/itemFrame.java
+++ Cookie Clicker/src/itemFrame.java
@@ -2,31 +2,29 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.net.URL;
 
-public class Building {
-	private int x, y;
-	private Image img;
+public class itemFrame{
+	
+	//add location attributes
+	private Image img; 	
 	private AffineTransform tx;
 
-	public Building(int x, int y, String i) {
-		this.x = x;
-		this.y = y;
-		img = getImage("/imgs/"+i+".png");
+	public itemFrame(int x, int y) {
+		img = getImage("/imgs/upgradeFrameOld.png");
 		tx = AffineTransform.getTranslateInstance(x, y);
-		init(x, y); 
-	
-
+		init(x, y); 				
 	}
-	
-	
+
 	
 	public void paint(Graphics g) {
 		//these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
 		
-		//call update to update the actual picture location
+		//call update to update the actualy picture location
 		update();
 		g2.drawImage(img, tx, null);
 
@@ -40,7 +38,7 @@ public class Building {
 	
 	private void init(double a, double b) {
 		tx.setToTranslation(a, b);
-		tx.scale(2, 2);
+		tx.scale(1, 1);
 	}
 
 	private Image getImage(String path) {
@@ -53,4 +51,5 @@ public class Building {
 		}
 		return tempImage;
 	}
+
 }
