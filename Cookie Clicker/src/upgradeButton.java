8404c5c32097284663db02a9b55ad7721ffa037c
1903994 <1903994@01-19766.pusd.dom>
2022-05-09 12:20:34 -0700

    test

--- Cookie Clicker/src/upgradeButton.java
+++ Cookie Clicker/src/upgradeButton.java
@@ -1,4 +1,65 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class upgradeButton {
+	
+	private int x, y;
+	private Image img;
+	private AffineTransform tx;
+
+	public upgradeButton(int x, int y, String i) {
+		this.x = x;
+		this.y = y;
+		img = getImage("/imgs/"+i+".png");
+		tx = AffineTransform.getTranslateInstance(x, y);
+		init(x, y); 
+	
+
+	}
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		//call update to update the actual picture location
+		update();
+		g2.drawImage(img, tx, null);
+
+	}
+	/* update the picture variable location */
+	private void update() {
+
+		
+		
+	}
+	
+	public void clicked1() {
+		tx.scale(0.95, 0.95);
+
+		
+	}
+	public void clicked2() {
+		tx.scale((1/0.95), (1/0.95));
+
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(1.5, 1.5);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
 
 }
