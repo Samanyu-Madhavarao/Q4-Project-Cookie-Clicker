141f192664ce98bd42e8958913663eb2e607cb9a
1903994 <1903994@01-19766.pusd.dom>
2022-05-18 12:59:32 -0700

    test

--- Cookie Clicker/src/Frame.java
+++ Cookie Clicker/src/Frame.java
@@ -858,7 +858,7 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 		if(x >= 900 && x <= 935 && y >= 80 && y <= 115 && !farmUpgrade2Bool && numCookies >= rarePrice) {
 			farmUpgrade2Bool = true;
 			numCookies -= rarePrice;
-			farmCPS *= 5;
+			farmCPS *= 5; 
 			numUpgrades++;
 		}
 		
