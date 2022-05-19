2a2d37d4558a98b38a8ae6049507ffabd1bbad1b
1913987 <1913987@01-19833.pusd.dom>
2022-05-19 13:22:17 -0700

    h

--- Cookie Clicker/src/Frame.java
+++ Cookie Clicker/src/Frame.java
@@ -639,7 +639,7 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 		if(numCookies > maxCookies) {
 			maxCookies = numCookies; 
 		}
-
+ 
 		if(numCookies == 20000000) {
 			Building temp = new Building(cycleX, cycleY, "GoldenCookie"); 
 			cycles.add(temp);
